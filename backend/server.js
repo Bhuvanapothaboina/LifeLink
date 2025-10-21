6272f9e73ea49bc102a1fc3df1c95b291eb23ebf
Bhuvanapothaboina <pbhuvana412@gmail.com>
2025-10-21 16:36:58 +0530

    Updated CORS origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({}));
+app.use(cors({origin:"https://vercel.com/bhuvanas-projects-c0b9f6e1/life-link/88tVP18UDfTF1aYswNaNKBVkXJCs",
+  credentials:true,
+}));
 
 // MongoDB connection
 mongoose
